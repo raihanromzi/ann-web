17f1528db4a788d2dc6eb0a352c5bace11183671
Raihan Romzi R <raihan.rakhman16@gmail.com>
2025-03-18 11:16:51 +0700

    feat: add favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,9 @@ export const metadata = {
   title: "Anugerah Avirama Nawasena 2024",
   description:
     "Menghargai Inovasi Berkelanjutan untuk Masa Depan yang Lebih Hijau",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
